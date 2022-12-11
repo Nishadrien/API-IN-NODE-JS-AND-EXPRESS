466a2774a39c6d8238f22a8430f7576217a3670d
Nishadrien <nishimirwadrien@gmail.com>
2022-12-11 16:30:56 +0200

    crud api

--- API/routes/products.js
+++ API/routes/products.js
@@ -1,39 +1,78 @@
 const express=require('express');
 const router=express.Router();
-
-router.get('/',(re,res,next)=>{
-    res.status(200).json({
-        message:'All products fetched successfully ',
-        
-    });
+const mongoose=require('mongoose');
+const Product=require('../models/products');
+router.get('/',(req,res,next)=>{
+Product.find().then((result)=>{
+   
+        res.status(200).json(result);  
+})
 });
-router.post('/',(re,res,next)=>{
-    res.status(201).json({
-        message:'it is post method of products ',
-        
+router.post('/',(req,res,next)=>{
+   
+    const product=new Product({
+_id:new mongoose.Types.ObjectId(),
+name:req.body.name,
+price:req.body.price
     });
+    product.save().then((result)=>{
+        console.log('inserted successfully');
+        res.status(201).json({
+            message:'it is post method of products ',
+            createdProduct:product
+            
+        });
+    })
+    .catch(err=>{
+        res.status(500).json({
+            Error:err
+        });
+    })
+   
 });
 router.get('/:productId',(req,res,next)=>{
     const id=req.params.productId;
-    if(id=='1234'){
+   Product.findById({_id:id})
+   .exec()
+   .then((result)=>{
+    
+    if(result){
+        console.log('One Element Fetched');
         res.status(200).json({
-            message:'it is first products in products tables',
-            id:id
-        })
+            ProductId:result 
+         })
     }
     else{
-        res.status(200).json({
-            message:'enter valid Id'
+        res.status(404).json({
+            message:'No data found for this '+id
         })
-    }});
+    }
+   }).catch(err=>{
+    console.log(err)
+    res.status(500).json({
+        error:err
+    })
+   })
+    });
     router.patch('/:productId',(req,res,next)=>{
-         res.status(200).json({
-         message:'Updated successfully'
-        });
+   
+       
     });
     router.delete('/:productId',(req,res,next)=>{
+       const id=req.params.productId;
+       Product.remove({_id:id})
+       .exec()
+       .then((result)=>{
+        console.log(result);
         res.status(200).json({
-        message:'deleted successfully'
+            message:'deleted successfully'
+        })
+       })
+       .catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        });
        });
    });
 module.exports=router;
--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express=require('express');
-const router=require('./API/routes/products');
-const routerorder=require('./API/routes/orders');
 const app=express();
-app.use('/products',router);
+const morgan=require('morgan');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+
+
+const routerproduct=require('./API/routes/products');
+const routerorder=require('./API/routes/orders');
+mongoose.connect('mongodb+srv://admin:AydamcDFhOXOyFwF@cluster0.hcj3zr6.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+})
+
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/products',routerproduct);
 app.use('/orders',routerorder);
+app.use((req,res,next)=>{
+    const error=new Error('Not found');
+    error.status=404;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
 module.exports=app;
